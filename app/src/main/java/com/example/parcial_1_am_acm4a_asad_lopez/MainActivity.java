00d10f38f7985b062c48ce320923046c277b4e80
seezelop <sebastianlopez1984@gmail.com>
2023-10-08 15:19:31 -0300

    Creando repositorio.

---
+++ app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.parcial_1_am_acm4a_asad_lopez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "parcial-1-am-acm4a-asad-lopez"
+include(":app")
