f9a16ee1da34604d9754a9520cc0fd02d45d782e
seezelop <sebastianlopez1984@gmail.com>
2023-10-17 11:27:03 -0300

    fix: I change btn "find by day" and btn "find"".

--- app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/MainActivity.java
@@ -3,14 +3,59 @@ package com.example.parcial_1_am_acm4a_asad_lopez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.PopupWindow;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner s;
+    private Spinner s2 = findViewById(R.id.opciones_actividad);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // prueba subida 2
+        s = findViewById(R.id.opciones_dia);
+        ArrayList<String> dias = new ArrayList<>();
+        dias.add("Lunes");//agrego los dias a la lista
+        dias.add("Martes");
+        dias.add("Miercoles");
+
+        ArrayList<String> actividades = new ArrayList<>();
+        actividades.add("Visitas guiadas");
+        actividades.add("Gastronomia");
+        actividades.add("Deporte");
+
+        //parametros: contexto, ubicacion de los items del spinner, arraylist
+        ArrayAdapter<String> adaptador = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, dias);
+
+
+        s.setAdapter(adaptador);//agregar adaptador al spinner
+
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String mensaje = (String) s.getAdapter().getItem(i);
+
+                Toast.makeText(MainActivity.this, "posicion: " + mensaje, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
