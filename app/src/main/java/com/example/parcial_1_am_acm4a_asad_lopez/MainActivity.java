83b09fead035bf2793d748209f911d39f066941e
MartinaAsad <lasupermartu15@gmail.com>
2023-10-17 12:18:49 -0300

    fix: the 2 spinners run and also the event on miBoton

--- app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -17,69 +18,49 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private Spinner s;
-    private Spinner s2 = findViewById(R.id.opciones_actividad);
+    private Button miBoton;
+    private Spinner spinner1;
+    private Spinner spinner2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            s= findViewById(R.id.opciones_dia);
+        miBoton = findViewById(R.id.btn_contenido_buscar);
+        spinner1 = findViewById(R.id.opciones_dia);
+        spinner2 = findViewById(R.id.opciones_actividad);
+
+        // arraylist primer spinner
         ArrayList<String> dias = new ArrayList<>();
-            dias.add("Lunes");//agrego los dias a la lista
-            dias.add("Martes");
-            dias.add("Miercoles");
+        dias.add("Lunes");
+        dias.add("Martes");
 
-            ArrayList<String> actividades=new ArrayList<>();
-            actividades.add("Visitas guiadas");
-            actividades.add("Gastronomia");
-            actividades.add("Deporte");
+        //arraylist segundo spinner
+        ArrayList<String> actividades = new ArrayList<>();
+        actividades.add("Visitas guiadas");
+        actividades.add("Gastronomia");
+        //actividades.add("Deportes");
 
-            //parametros: contexto, ubicacion de los items del spinner, arraylist
-            ArrayAdapter<String> adaptador=new ArrayAdapter(this,
-                    android.R.layout.simple_spinner_item, dias);
-
-            ArrayAdapter<String> adaptador2=new ArrayAdapter(this,
-                android.R.layout.simple_spinner_item, actividades);
-
-            s.setAdapter(adaptador);//agregar adaptador al spinner
-            s2.setAdapter(adaptador2);
-            //s.setGravity(Gravity.BOTTOM);
-
-            s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                    String mensaje=(String)s.getAdapter().getItem(i);
-
-                    Toast.makeText(MainActivity.this, "posicion: "+mensaje, Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onNothingSelected(AdapterView<?> adapterView) {
-
-                }
-            });
-
-            s2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                    String mensaje=(String)s.getAdapter().getItem(i);
-
-                    Toast.makeText(MainActivity.this, "posicion: "+mensaje, Toast.LENGTH_SHORT).show();
-
-                }
-
-                @Override
-                public void onNothingSelected(AdapterView<?> adapterView) {
-
-                }
-            });
+        // 1 adaptador por cada spinner
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, dias);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, actividades);
 
 
+        spinner1.setAdapter(adapter1);
+        spinner2.setAdapter(adapter2);
 
-
-
-
-        }
+        miBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Opcioj seleccionada", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
+
+
+
+
+
+
