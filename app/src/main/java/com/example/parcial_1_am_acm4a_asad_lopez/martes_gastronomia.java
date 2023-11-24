9649f2c31f93893c091c94ad25f9d0b6b14ab84b
seezelop <sebastianlopez1984@gmail.com>
2023-11-24 20:34:25 -0300

    Fix: I update activity_martes_gastronomia.xml, martes_gastronomia.java on my branch

--- app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/martes_gastronomia.java
+++ app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/martes_gastronomia.java
@@ -1,7 +1,6 @@
 package com.example.parcial_1_am_acm4a_asad_lopez;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -9,40 +8,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class martes_gastronomia extends AppCompatActivity {
-    TextView titulo_1;
-    TextView hora_dias_colon;
-    TextView texto_colon;
-    TextView titulo_2;
-    TextView descripcion_buses;
-    TextView horario_bus;
+    private TextView tituloGastronomia;
+    private TextView horaGastronomia;
+    private TextView descripcionGastronomia;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_martes_gastronomia);
 
-        List<String> titulos=new ArrayList<String>();
-        titulos.add("Conocé las majoeres ofertas gastronomicas");
-        titulos.add("Restaurantes tematicos");
+        List<String> titulos = new ArrayList<>();
+        titulos.add("Experiencia Gastronómica en el Centro");
+        // Agrega más títulos según sea necesario
 
-        titulo_1=findViewById(R.id.titulo_1);
-        titulo_1.setText(titulos.get(0));/*aca se agrega titulo resto*/
-        titulo_2=findViewById(R.id.titulo_2);
-        titulo_2.setText(titulos.get(1));/*aca se agrega titulo  resto2*/
+        tituloGastronomia = findViewById(R.id.titulo_1);
+        tituloGastronomia.setText(titulos.get(0)); /* Agrega el título de la experiencia gastronómica */
 
-        List<String> horarios=new ArrayList<String>();
-        horarios.add("De lunes a viernes, de 10 a 16:45 h. Sábado y domingo, de 11 a 16:45 h.");
-        horarios.add("Todos los días, de 9 a 17 h. (el amarillo) y de 9:30 a 18 h (el rojo)");
-        hora_dias_colon=findViewById(R.id.hora_dias_colon);
-        hora_dias_colon.setText(horarios.get(0));/*aca se agrega horarios y dias del colon*/
-        horario_bus=findViewById(R.id.horario_dias_buses);
-        horario_bus.setText(horarios.get(1));/*aca se agrega horarios y dias del bus*/
+        List<String> horarios = new ArrayList<>();
+        horarios.add("Martes y jueves, de 19:00 a 22:00 h");
+        // Agrega más horarios según sea necesario
 
-        List<String> descripciones=new ArrayList<String>();
-        descripciones.add("Recorré los rincones del Teatro Colón con quienes pueden enseñarte todos sus secretos e historia. Van a guiarte por la sala, el foyer principal, la galería de bustos y el salón dorado , y te contarán los detalles de la arquitectura de este magnífico edificio. El paseo dura aproximadamente unos 50 minutos.");
-        descripciones.add("Una gran forma de recorrer los barrios más emblemáticos de la Ciudad y tener una gran vista panorámica. Dos circuitos, amarillo y rojo, para que puedas apreciar lugares históricos, espacios culturales y zonas únicas. Podés ascender y descender de los buses todas las veces que desees. Consultá la promoción que ofrecen en Vacaciones de Invierno");
-        texto_colon=findViewById(R.id.texto_colon);
-        texto_colon.setText(descripciones.get(0));/*aca se agrega la descripcion del colon*/
-        descripcion_buses=findViewById(R.id.descripcion_buses);
-        descripcion_buses.setText(descripciones.get(1));/*aca se agrega la descripcion de los buses*/
+        horaGastronomia = findViewById(R.id.hora_dias_colon);
+        horaGastronomia.setText(horarios.get(0)); /* Agrega los horarios y días de la experiencia gastronómica */
+
+        List<String> descripciones = new ArrayList<>();
+        descripciones.add("Descubre los sabores auténticos de la gastronomía local en nuestro recorrido por los restaurantes del centro. Degusta platos tradicionales y experimenta con nuevas creaciones culinarias. ¡Una experiencia única para los amantes de la buena comida!");
+        // Agrega más descripciones según sea necesario
+
+        descripcionGastronomia = findViewById(R.id.texto_colon);
+        descripcionGastronomia.setText(descripciones.get(0)); /* Agrega la descripción de la experiencia gastronómica */
     }
 }
+
