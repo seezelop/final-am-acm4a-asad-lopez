c215b9c9d8231996f67348c28fbcea6f30920648
MartinaAsad <lasupermartu15@gmail.com>
2023-12-09 14:59:01 -0300

    feat: I add the login with firebase authentication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id ("com.google.gms.google-services")
 }
 
 android {
@@ -35,6 +36,12 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("androidx.navigation:navigation-fragment:2.3.5")
     implementation ("androidx.navigation:navigation-ui:2.3.5")
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/logica_login.java
+++ app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/logica_login.java
@@ -1,15 +1,86 @@
 package com.example.parcial_1_am_acm4a_asad_lopez;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class logica_login extends AppCompatActivity {
+
+    ImageView imageView;
+    EditText usuario;
+    EditText contra;
+    Button boton_inicio;
+
+    FirebaseAuth fAuth;//uso del firebase authentication
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        fAuth=FirebaseAuth.getInstance();//se crea una instancia
 
+        imageView=findViewById(R.id.imagen_logo_ba);
+        usuario=findViewById(R.id.edittext_usuario);
+        contra=findViewById(R.id.edittext_contrasena);
+        boton_inicio=findViewById(R.id.boton_login);
+
+
+
+
+
+    }
+
+    public void iniciar_sesion(View view) {
+        //obtengo lo que el usuario ingreso como mail
+        String emailUsuario= usuario.getText().toString().trim();
+
+        //obtengo lo que el usuario ingreso como contrasena
+        String contraUsuario= contra.getText().toString().trim();
+
+        Toast.makeText(logica_login.this, "evento apretado", Toast.LENGTH_SHORT).show();
+        //condicional para saber si los campos estan vacios
+        if (emailUsuario.isEmpty() || contraUsuario.isEmpty()) {
+            Toast.makeText(logica_login.this, "Complete los campos en blanco", Toast.LENGTH_SHORT).show();
+        } else {
+            loginUsuarios(emailUsuario, contraUsuario);
+        }
+
+
+    }
+    private void loginUsuarios(String emailUsuario, String contraUsuario) {
+        fAuth.signInWithEmailAndPassword(emailUsuario, contraUsuario).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                //pregunta si hay una sesion
+                if(task.isSuccessful()){
+                    finish();
+                    startActivity(new Intent(logica_login.this, MainActivity.class));
+                    Toast.makeText(logica_login.this, "Bienvenido!", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(logica_login.this, "error" + task.getException(), Toast.LENGTH_SHORT).show();
+                }
+            }
+            //linea 67 muestra los errores al loguearse
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(logica_login.this, "error al iniciar sesion", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
