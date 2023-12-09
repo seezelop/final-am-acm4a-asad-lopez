d4f41eb1af581c67cbf66da81fe8076913926b3c
MartinaAsad <lasupermartu15@gmail.com>
2023-12-09 13:04:55 -0300

    feat: I add the interface of the login

--- app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.parcial_1_am_acm4a_asad_lopez;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -13,10 +19,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -32,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //setupNavegacion();
          TextView contenido_d = findViewById(R.id.contenido_dia);
          contenido_d.setText(R.string.contenido_dia);
 
@@ -94,6 +104,30 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    /*evento para que al apretar la casita,vaya a la interfaz de inicio de sesion*/
+
+
+
+    public void ir_a_inicio_sesion(View view){
+        /*Fragment inicioSesionFragment = new inicioSesion();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.contenedor_menu, inicioSesionFragment); // Reemplaza R.id.contenedor_fragment con el ID de tu contenedor de fragmentos
+        fragmentTransaction.addToBackStack(null); // Opcional: agregar a la pila de retroceso
+        fragmentTransaction.commit();*/
+        Intent intent=new Intent(MainActivity.this, logica_login.class);
+        startActivity(intent);
+    }
+    /*metodo para la configuracion de la navegacion*/
+    /*private void setupNavegacion(){
+        BottomNavigationView bottomNavigationView= findViewById(R.id.botonNavegacion);
+        NavHostFragment navHostFragment= (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.contenedor_menu);
+        NavigationUI.setupWithNavController(
+                bottomNavigationView, navHostFragment.getNavController()
+
+        );
+    }*/
 }
 
 
---
+++ app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/logica_login.java
@@ -0,0 +1,15 @@
+package com.example.parcial_1_am_acm4a_asad_lopez;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class logica_login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
