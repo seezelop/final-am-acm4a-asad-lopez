a3068c24fd1730ae49a964826c10bbbd5412f84b
seezelop <sebastianlopez1984@gmail.com>
2023-12-13 16:58:06 -0300

    Update: I update my partner's changes.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id ("com.google.gms.google-services")
 }
 
 android {
@@ -33,7 +34,16 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.navigation:navigation-fragment:2.3.5")
+    implementation ("androidx.navigation:navigation-ui:2.3.5")
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")//comentario
 }
--- app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.parcial_1_am_acm4a_asad_lopez;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -13,10 +19,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -25,11 +34,14 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spinner1;
     private Spinner spinner2;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //setupNavegacion();
         TextView contenido_d = findViewById(R.id.contenido_dia);
         contenido_d.setText(R.string.contenido_dia);
 
@@ -42,14 +54,16 @@ public class MainActivity extends AppCompatActivity {
         dias.add("Lunes");
         dias.add("Martes");
 
-        // arraylist segundo spinner
+        //arraylist segundo spinner
         ArrayList<String> actividades = new ArrayList<>();
         actividades.add("Visitas guiadas");
         actividades.add("Gastronomia");
+        //actividades.add("Deportes");
 
         // 1 adaptador por cada spinner
         ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, dias);
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, actividades);
+
 
         spinner1.setAdapter(adapter1);
         spinner2.setAdapter(adapter2);
@@ -57,29 +71,66 @@ public class MainActivity extends AppCompatActivity {
         miBoton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Captura la opción seleccionada en cada spinner
-                CharSequence diaSeleccionado = (CharSequence) spinner1.getSelectedItem();
-                CharSequence actividadSeleccionada = (CharSequence) spinner2.getSelectedItem();
+                //aca capturo la opcion seleccionada en cada spinner
+                CharSequence diaSeleccionado= (CharSequence) spinner1.getSelectedItem();
+                CharSequence actividadSeleccionada=(CharSequence) spinner2.getSelectedItem();
 
-                // Verifica las opciones seleccionadas
-                if (diaSeleccionado.equals("Lunes") && actividadSeleccionada.equals("Visitas guiadas")) {
+
+                //si selecciono lunes como dia y visitas guiadas como actividad
+                if(diaSeleccionado.equals("Lunes") && actividadSeleccionada.equals("Visitas guiadas")){
                     Intent intent = new Intent(MainActivity.this, lunes_visitas.class);
                     startActivity(intent);
-                } else if (diaSeleccionado.equals("Martes") && actividadSeleccionada.equals("Gastronomia")) {
+                }  //si selecciono martes y gastronomia
+                else if (diaSeleccionado.equals("Martes") && actividadSeleccionada.equals("Gastronomia")) {
                     Intent intent = new Intent(MainActivity.this, martes_gastronomia.class);
                     startActivity(intent);
                 } else if (diaSeleccionado.equals("Martes") && actividadSeleccionada.equals("Visitas guiadas")) {
                     Intent intent = new Intent(MainActivity.this, martes_visitas.class);
                     startActivity(intent);
-                } else {
-                    // Otras opciones o manejo de error
+                } else{
+                    // Toast.makeText(this, "seleccione una opcion valida").show();
                     Toast toast;
                     int duracion = Toast.LENGTH_SHORT;
-                    String mensaje = "No hay actividades en el día seleccionado";
-                    toast = Toast.makeText(getApplicationContext(), mensaje, duracion);
+                    String mensaje="No hay actividades en el dia seleccionado";
+                    toast = Toast.makeText(getApplicationContext(),mensaje, duracion);
                     toast.show();
+
                 }
+
+
+
             }
         });
+
+
     }
+
+    /*evento para que al apretar la casita,vaya a la interfaz de inicio de sesion*/
+
+
+
+    public void ir_a_inicio_sesion(View view){
+        /*Fragment inicioSesionFragment = new inicioSesion();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.contenedor_menu, inicioSesionFragment); // Reemplaza R.id.contenedor_fragment con el ID de tu contenedor de fragmentos
+        fragmentTransaction.addToBackStack(null); // Opcional: agregar a la pila de retroceso
+        fragmentTransaction.commit();*/
+        Intent intent=new Intent(MainActivity.this, logica_login.class);
+        startActivity(intent);
+    }
+    /*metodo para la configuracion de la navegacion*/
+    /*private void setupNavegacion(){
+        BottomNavigationView bottomNavigationView= findViewById(R.id.botonNavegacion);
+        NavHostFragment navHostFragment= (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.contenedor_menu);
+        NavigationUI.setupWithNavController(
+                bottomNavigationView, navHostFragment.getNavController()
+
+        );
+    }*/
 }
+
+
+
+
+
