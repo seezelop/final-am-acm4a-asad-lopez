d3a77266b8ef60aac6aedc712ce57c7ed9a5348f
MartinaAsad <lasupermartu15@gmail.com>
2023-12-14 20:47:06 -0300

    feat: I add sebastian's part

--- app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/martes_gastronomia.java
+++ app/src/main/java/com/example/parcial_1_am_acm4a_asad_lopez/martes_gastronomia.java
@@ -1,10 +1,11 @@
 package com.example.parcial_1_am_acm4a_asad_lopez;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class martes_gastronomia extends AppCompatActivity {
 
@@ -24,6 +25,17 @@ public class martes_gastronomia extends AppCompatActivity {
         textView.setText(getString(R.string.destacado_gastronomia));
 
         imageView.setImageResource(R.drawable.food);
+
+        // Obtén una referencia al ImageView
+        //ImageView imageView = findViewById(R.id.imagenGastro);
+
+// URL de la imagen que deseas descargar
+        String urlImagen = "https://dam.ngenespanol.com/wp-content/uploads/2018/11/5-gastronomia-paises-mundo.png";
+
+// Usa Glide para cargar la imagen en el ImageView
+        Glide.with(this)
+                .load(urlImagen)
+                .into(imageView);
 
 
 
